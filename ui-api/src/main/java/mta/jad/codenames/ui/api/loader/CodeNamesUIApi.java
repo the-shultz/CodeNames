5aea9a649dc984297d24f6389a1f3fb3b5e5df25
Aviad Cohen <aviadco@mta.ac.il>
2024-02-09 15:04:44 +0200

    refactor loader

--- ui-api/src/main/java/mta/jad/codenames/ui/api/loader/CodeNamesUIApi.java
+++ ui-api/src/main/java/mta/jad/codenames/ui/api/loader/CodeNamesUIApi.java
@@ -1,5 +1,6 @@
 package mta.jad.codenames.ui.api.loader;
 
+import lombok.Getter;
 import mta.jad.codenames.ui.api.dashboard.GamesDashboard;
 
 import java.io.File;
@@ -9,11 +10,12 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.MissingResourceException;
 import java.util.function.Consumer;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+@Getter
 public enum CodeNamesUIApi {
     INSTANCE;
 
@@ -22,10 +24,49 @@ public enum CodeNamesUIApi {
     public void init() throws IOException {
         String userDir = System.getProperty("user.dir");
         System.out.println("current working directory : " + userDir);
-        File workingDirectory=new File("C:\\aviad\\courses\\Java Application Development\\exercises\\2024 - B - Code Names\\code-names\\ui-api-mock-impl\\target");
+        init(userDir);
+    }
+
+    public void init(String workingDirectoryPath) throws IOException {
+        File workingDirectory = new File(workingDirectoryPath);        
         File[] files = workingDirectory.listFiles((dir, name) -> name.endsWith(".jar"));
+
+        if (files == null || files.length == 0) {
+            System.out.println("No jar files found in the working directory (" + workingDirectoryPath + ")! Exiting...");
+            throw new RuntimeException("No jar files found in the working directory [" + workingDirectoryPath + "]! Exiting...");
+        } else {
+            System.out.println("Found " + files.length + " jar files in the working directory (" + workingDirectoryPath + "):");
+        }
+
+        ClassesAndJars result = processJars(files);
+
+        URLClassLoader urlClassLoader = new URLClassLoader(result.urls.toArray(new URL[0]));
+
+        locateGameDashboardImpl(urlClassLoader, result);
+
+        urlClassLoader.close();
+
+    }
+
+    private void locateGameDashboardImpl(URLClassLoader urlClassLoader, ClassesAndJars result) {
+
+        Consumer<String> GameDashboardExtractor = findApiImplementationWrapper(urlClassLoader, GamesDashboard.class, apiImpl -> {
+            gamesDashboard = apiImpl;
+            System.out.println("GamesDashboard implementation found: " + apiImpl.getClass().getName());
+        });
+
+        result.classes.forEach(GameDashboardExtractor);
+
+        if (gamesDashboard == null) {
+            System.out.println("No GamesDashboard implementation found! Exiting...");
+            throw new MissingResourceException("No GamesDashboard implementation found!", GamesDashboard.class.getName(), "");
+        }
+    }
+
+    private static ClassesAndJars processJars(File[] files) {
         ArrayList<URL> urls = new ArrayList<>(files.length);
-        List<String> classes = Arrays
+        List<String> classes =
+            Arrays
                 .stream(files)
                 .map(file -> {
                     System.out.println("Processing: " + file.getName());
@@ -33,12 +74,11 @@ public enum CodeNamesUIApi {
                         URL url=file.toURI().toURL();
                         urls.add(url);
 
-                        return
-                                jar
-                                        .stream()
-                                        .filter(jarEntry -> jarEntry.getName().endsWith(".class"))
-                                        .map(jarEntry -> jarEntry.getName().replace("/", ".").replace(".class", ""))
-                                        .collect(Collectors.toList());
+                        return jar
+                                .stream()
+                                .filter(jarEntry -> jarEntry.getName().endsWith(".class"))
+                                .map(jarEntry -> jarEntry.getName().replace("/", ".").replace(".class", ""))
+                                .collect(Collectors.toList());
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
@@ -46,22 +86,20 @@ public enum CodeNamesUIApi {
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
 
-        classes.forEach(System.out::println);
+        //classes.forEach(System.out::println);
 
-        URLClassLoader urlClassLoader = new URLClassLoader(urls.toArray(new URL[0]));
+        return new ClassesAndJars(urls, classes);
+    }
 
-        Consumer<String> GameDashboardExtractor = findApiImplementationWrapper(urlClassLoader, GamesDashboard.class, apiImpl -> {
-            gamesDashboard = apiImpl;
-            System.out.println("GamesDashboard implementation found: " + apiImpl.getClass().getName());
-        });
+    private static class ClassesAndJars {
 
-        classes.forEach(GameDashboardExtractor);
-        if (gamesDashboard == null) {
-            System.out.println("No GamesDashboard implementation found! Exiting...");
+        public final ArrayList<URL> urls;
+        public final List<String> classes;
+
+        public ClassesAndJars(ArrayList<URL> urls, List<String> classes) {
+            this.urls = urls;
+            this.classes = classes;
         }
-
-        urlClassLoader.close();
-
     }
 
     private <T> Consumer<String> findApiImplementationWrapper(URLClassLoader urlClassLoader, Class<T> api, Consumer<T> onSuccess) {
@@ -71,7 +109,6 @@ public enum CodeNamesUIApi {
     private <T> void findImplementingInterface(URLClassLoader urlClassLoader, String className, Class<T> api, Consumer<T> onSuccess) {
         try {
             Class<?> clazz = urlClassLoader.loadClass(className);
-            //Class[] interfaces=clazz.getInterfaces();
             if (api.isAssignableFrom(clazz)) {
                 T apiInstance = (T)clazz.newInstance();
                 onSuccess.accept(apiInstance);
@@ -82,11 +119,8 @@ public enum CodeNamesUIApi {
 
     }
 
-    public GamesDashboard getGamesDashboard() {
-        return gamesDashboard;
-    }
-
     public static void main(String[] args) throws IOException {
-        CodeNamesUIApi.INSTANCE.init();
+        String workDir = "C:\\aviad\\courses\\Java Application Development\\exercises\\2024 - B - Code Names\\code-names\\ui-api-mock-impl\\target";
+        CodeNamesUIApi.INSTANCE.init(workDir);
     }
 }
