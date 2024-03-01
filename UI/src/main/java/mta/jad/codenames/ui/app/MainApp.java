a056322c5a687f5f195d5cfd04966d3bc50454d5
Aviad Cohen <aviadco@mta.ac.il>
2024-03-01 18:54:54 +0200

    add mock active game documentation

--- UI/src/main/java/mta/jad/codenames/ui/app/MainApp.java
+++ UI/src/main/java/mta/jad/codenames/ui/app/MainApp.java
@@ -11,6 +11,43 @@ import mta.jad.codenames.ui.api.loader.CodeNamesUIApi;
 import java.net.URL;
 import java.util.Optional;
 
+/*
+
+### init from real implementation
+
+        api impl location can be supplied externally via -DapiImplFolder=... or defaults to working directory
+        String implLocation = Optional
+                .ofNullable(System.getProperty("apiImplFolder"))
+                .orElse(System.getProperty("user.dir"));
+
+        initialize the API instances to be used by the UI. has to be called once upon startup.
+        in case fails to locate API, throws informative exceptions.
+        CodeNamesUIApi.INSTANCE.init(implLocation);
+
+### init using mocks
+
+        // call the init with an instance of the mock implementation:
+        CodeNamesUIApi.INSTANCE.init(new LoginMock(), new GamesDashboardMock());
+
+        for ease of use each mock implementation has a builder that allows to set the desired behavior of the mock.
+        you can also use MockApiFactory to create a mock with a predefined behavior for either user or admin,
+        e.g.
+        Admin:
+        CodeNamesUIApi.INSTANCE.init(MockApiFactory.CreateSuccessfulLogin(), MockApiFactory.CreateAdminGamesDashboard(), MockApiFactory.createAdminActiveGame());
+        User:
+        CodeNamesUIApi.INSTANCE.init(MockApiFactory.CreateSuccessfulLogin(), MockApiFactory.CreateUserGamesDashboard(), MockApiFactory.createPlayerActiveGame());
+
+### using the API:
+
+        once initialized successfully, simply gain access to the API...
+
+        CodeNamesUIApi.INSTANCE.getGamesDashboard().getGameDetails("gameName", fullGameDetails -> {
+            System.out.println("fullGameDetails = " + fullGameDetails);
+        }, fullGameDetailsFailure -> {
+            System.out.println("fullGameDetailsFailure = " + fullGameDetailsFailure);
+        });
+
+ */
 public class MainApp extends Application {
 
     @Override
@@ -25,14 +62,6 @@ public class MainApp extends Application {
         // in case fails to locate API, throws informative exceptions.
         //CodeNamesUIApi.INSTANCE.init(implLocation);
 
-/*
-        // once initialized successfully, simple gain access to the API...
-        CodeNamesUIApi.INSTANCE.getGamesDashboard().getGameDetails("gameName", fullGameDetails -> {
-            System.out.println("fullGameDetails = " + fullGameDetails);
-        }, fullGameDetailsFailure -> {
-            System.out.println("fullGameDetailsFailure = " + fullGameDetailsFailure);
-        });
-*/
 
         URL resource = getClass().getResource("/app/form/LoginForm.fxml");
         assert resource != null;
