39418750a416449f1db7ec2ee3ad23d005e64c18
Aviad Cohen <aviadc@liveperson.com>
2024-03-02 16:36:46 +0200

    initial wiring for games updates registrations

--- UI/src/main/java/mta/jad/codenames/ui/app/DashboardController.java
+++ UI/src/main/java/mta/jad/codenames/ui/app/DashboardController.java
@@ -1,10 +1,12 @@
 package mta.jad.codenames.ui.app;
 
+import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import mta.jad.codenames.ui.api.loader.CodeNamesUIApi;
 import mta.jad.codenames.ui.app.components.TeamCard;
 
 public class DashboardController {
@@ -20,6 +22,13 @@ public class DashboardController {
 
         //TODO: Testing, remove later
         hboxTeams.getChildren().addAll(new TeamCard(), new TeamCard(), new TeamCard(), new TeamCard());
+
+        //TODO: update with respond to games data
+        CodeNamesUIApi.INSTANCE.getGamesDashboard().registerLightweightGameDetailsCallback(gameDetails ->
+                Platform.runLater(() -> {
+                    System.out.println("gameDetails = " + gameDetails);
+                }
+        ));
     }
 
     public void setAdminMode(boolean adminMode) {
